1660f5cb51e90c11b50553da3fe3392a11448600
Willem Bressers <dhr.bressers@gmail.com>
2021-10-16 13:55:47 +0200

    randomizer

--- src/random_output.py
+++ src/random_output.py
@@ -1,12 +1,13 @@
 import os
 import random
 
-options = [0, 1, 2]
+f = open("willem.log",'w')
+try:
+	while True:
+		output = str(random.choice([0, 1, 2]))
 
-while True:
-	output = str(random.choice([0, 1, 2]))
-
-	# print(output)
-	f = open("willem.log",'w')
-	f.write(output)
+		# print(output)
+		f.seek(0)
+		f.write(output)
+finally:
 	f.close()
---
+++ willem.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# When started, simply print the contents of the test log file.
+while : ; do
+  cat willem.log
+done
