ad47907e21822b1534f222c7df3cc69cb7d079a3
Tim de Klijn <timdeklijn@gmail.com>
2021-10-16 17:53:03 +0200

    Clean up pear

--- src/pears.py
+++ src/pears.py
@@ -1,9 +1,33 @@
+import argparse
 from pathlib import Path
+
 import depthai as dai
 import cv2.cv2 as cv2
 
-# TODO: We should set this as a command line argument
 SHOW_OUTPUT = False
+LOG_FNAME = "joep.log"
+
+
+def draw_frame(imgFrame, pos):
+    frame = imgFrame.getCvFrame()
+    return cv2.putText(
+        frame,
+        f"pos: {pos}",
+        (10, 30),
+        cv2.FONT_HERSHEY_TRIPLEX,
+        0.6,
+        (255, 255, 255),
+    )
+
+
+def map_pos(det):
+    xmean = (det.xmax + det.xmin) / 2
+    if xmean < 0.33:
+        return "1"
+    elif 0.33 < xmean < 0.66:
+        return "2"
+    else:
+        return "3"
 
 
 def run() -> None:
@@ -43,7 +67,7 @@ def run() -> None:
     camRgb.preview.link(nn.input)
     nn.out.link(nnOut.input)
 
-    f = open("joep.log", "w")
+    f = open(LOG_FNAME, "w")
 
     # =================================================================================
     # RUN
@@ -54,7 +78,7 @@ def run() -> None:
         # Connect to device and start pipeline
         with dai.Device(pipeline) as device:
 
-            # Output queues will be used to get the rgb frames and nn data from the 
+            # Output queues will be used to get the rgb frames and nn data from the
             # outputs defined above
             qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
             qDet = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
@@ -80,47 +104,24 @@ def run() -> None:
                 # a "0", "1", "2", "3" depending on the location of the detected object
                 # in the screen. The coordinates are between 0.0 and 1.0.
                 if detected:
-                    xmean = (det.xmax + det.xmin) / 2
-                    if xmean < 0.33:
-                        pos = "1"
-                    elif 0.33 < xmean < 0.66:
-                        pos = "2"
-                    else:
-                        pos = "3"
+                    pos = map_pos(det)
                 else:
                     # No detection
                     pos = "0"
 
-                # TODO: For now output is not accessed: this will be used (renamed?) to
-                #   indicate if an object is far away or close by.
-                if detected:
-                    output = "1"
-                else:
-                    output = "0"
-
                 # Go the the beginning of the output file and write the output to the
                 # first line.
+                #
+                # TODO: replace the "1" in the input string with some number that will
+                #   report the depth of the detected object.
                 f.seek(0)
                 output_string = f"{pos}1"
                 f.write(output_string)
 
                 if SHOW_OUTPUT:
                     imgFrame = qRgb.get()
-                    frame = imgFrame.getCvFrame()
-
-                    cv2.putText(
-                        frame,
-                        f"pos: {pos}",
-                        (
-                            10,
-                            30,
-                        ),
-                        cv2.FONT_HERSHEY_TRIPLEX,
-                        0.6,
-                        (255, 255, 255),
-                    )
-
-                    cv2.imshow("PEAR", frame)
+                    # Write the position on the frame
+                    cv2.imshow("PEAR", draw_frame(imgFrame, pos))
 
                 if cv2.waitKey(1) == ord("q"):
                     break
@@ -131,4 +132,7 @@ def run() -> None:
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--show", dest="SHOW_OUTPUT", action="store_true")
+    parser.parse_args()
     run()
